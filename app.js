ef4bc7c337699ce1106ebcdc6d60c385b53d6194
JamesCarrollRob <46402913+JamesCarrollRob@users.noreply.github.com>
2022-09-05 16:03:41 -0400

    added roundsPlayed feature, buttons, and new branch

--- app.js
+++ app.js
@@ -1,31 +1,21 @@
  const selections = ["rock", "paper", "scissors"];
  var computerChoice = getComputerChoice();
- var playerSelection = prompt("Choose paper, rock or scissors. ");
-// var computerVictory = console.log(computerChoice + " beats " + playerSelection + " ! I win! ");
-// var playerVictory = console.log(playerSelection + " beats " + computerChoice + "! You win!");
  
-//  var computerVictory = alert(computerChoice + " beats " + playerSelection + " ! I win! ");
-//  var playerVictory = alert(playerSelection + " beats " + computerChoice + "! You win!");
+ var roundsPlayed = 0;
 
-//returns a random item from computerChoice array
 function getComputerChoice(){
-    //  let computerChoice = ["Rock", "Paper", "Scissors"]
-    //  for (let i=0; i<computerChoice.length; i++){
-    //    console.log(computerChoice[i]);
-    //  }
+    
     return selections[Math.floor(Math.random()*selections.length)];
    
 
  }
  console.log(computerChoice);
- console.log(playerSelection);
+ 
 
 function playRound()
 
 {
-//     var computerVictory = console.log(computerChoice + " beats " + playerSelection + " ! I win! ");
-// var playerVictory = console.log(playerSelection + " beats " + computerChoice + "! You win!");
-    
+  var playerSelection = prompt("Choose paper, rock or scissors. ");
 
   if(playerSelection === computerChoice){
     console.log("Tie! No winner!");
@@ -36,6 +26,12 @@ function playRound()
     console.log ("playerVictory");
 }
 
-}
+ roundsPlayed += 1;
+ console.log(playerSelection);
+ console.log(roundsPlayed);
+  
+ }
+ 
 
- playRound();
+while(roundsPlayed < 5){playRound();}
+ 
