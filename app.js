ded3c1b7f3af28a7a82ef6d5e84f41ebe4759934
JamesCarrollRob <46402913+JamesCarrollRob@users.noreply.github.com>
2022-09-05 17:05:11 -0400

    fixed computerChoice bug, round no longer counts if its a tie

--- app.js
+++ app.js
@@ -1,38 +1,44 @@
  const selections = ["rock", "paper", "scissors"];
  const playBtn = document.querySelector("#play-round");
- var computerChoice = getComputerChoice();
+//  var computerChoice = getComputerChoice();
  var playerScore = 0;
  var computerScore = 0;
  var roundsPlayed = 0;
 
+
+ //
 function getComputerChoice(){
     
     return selections[Math.floor(Math.random()*selections.length)];
-   
+    
 
  }
- console.log(computerChoice);
+ 
  
 
 function playRound()
 
 {
   var playerSelection = prompt("Choose paper, rock or scissors. ");
+  var computerChoice = getComputerChoice();
 
   if(playerSelection === computerChoice){
     console.log("Tie! No winner!");
 }
   else if(playerSelection === "rock" && computerChoice === "paper" || playerSelection === "paper" && computerChoice === "scissors" || playerSelection === "scissors" && computerChoice === "rock") {
     computerScore += 1;
-    
+    roundsPlayed += 1;
     console.log("computer Victory");
 } else {
     playerScore += 1;
+    roundsPlayed += 1;
     console.log ("playerVictory");
 }
 
- roundsPlayed += 1;
+//  roundsPlayed += 1;
+//  getComputerChoice();
  console.log(playerSelection);
+ console.log(computerChoice);
  console.log(roundsPlayed);
  console.log( "Computer: " + computerScore);
  console.log( "Player: " + playerScore);
@@ -40,7 +46,9 @@ function playRound()
  }
  
 playBtn.addEventListener('click', () => {
-  while(roundsPlayed < 5){playRound();}
+  while(roundsPlayed < 5){
+   
+    playRound();}
 })
 
  
